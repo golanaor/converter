8aaa8b9159d192eeb8de8f1969be3933b1ff323e
golanaor <36920962+golanaor@users.noreply.github.com>
2019-11-24 22:38:30 +0200

    removed golda-api

--- src/converter/App.java
+++ src/converter/App.java
@@ -1,12 +1,9 @@
 package converter;
+
 import okhttp3.Response;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.io.UnsupportedEncodingException;
+
 import java.util.Scanner;
 
 public class App {
@@ -19,25 +16,22 @@ public class App {
         System.out.println("Enter to currency");
         String to = input2.nextLine();
 
-        //String url ="https://currency-exchange.p.rapidapi.com/exchange?q=1.0&from="+from+"&to="+to;
-        String url = "https://golda-go.herokuapp.com/get/branch" ;
+        String url = "https://currency-exchange.p.rapidapi.com/exchange?q=1.0&from=" + from + "&to=" + to;
 
         OkHttpClient client = new OkHttpClient();
 
         Request request = new Request.Builder()
-                 .url(url)
-                 .get()
-                 .build();
-                /*.url(url)
+                .url(url)
                 .get()
                 .addHeader("x-rapidapi-host", "currency-exchange.p.rapidapi.com")
                 .addHeader("x-rapidapi-key", "671674ff94msha586be03790d216p199e38jsn2b227d6bfdab")
-                .build();*/
+                .build();
 
         try {
             Response response = client.newCall(request).execute();
             System.out.println(response);
-            System.out.println(response.body().string());
+            if (response.body().string() != null)
+                System.out.println(response.body().string());
         } catch (Exception e) {
             System.out.println("not working");
             System.out.println(e.getMessage());
